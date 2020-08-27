5b63d69e87be79852a153879f8e089687f3845a4
Jack <wilkinsonjthomas@gmail.com>
2020-08-27 00:07:03 -0500

    Adding for edit functionality

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('resume/view', views.work_experience_list, name='work_experience_list'),
+    path('work_experience/new', views.work_experience_new, name='work_experience_new'),
 ]
